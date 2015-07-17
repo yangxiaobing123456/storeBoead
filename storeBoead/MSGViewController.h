4071526f60cd4c47922f59049da2a7127d45e922
yangxiaobing0725 <2797422923@qq.com>
2015-07-17 14:39:19 +0800

    创建故事版

---
+++ storeBoead/MSGViewController.h
@@ -0,0 +1,13 @@
+//
+//  MSGViewController.h
+//  storeBoead
+//
+//  Created by kpkj-ios on 15/7/15.
+//  Copyright (c) 2015年 kpkj-ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSGViewController : UIViewController
+
+@end
